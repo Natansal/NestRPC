36f68de37a9ea31760b1d406c8ccdf93d6e11fa0
Natan Salmon <Natan@weme.community>
2025-08-16 23:58:20 +0300

    build progress

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -1,36 +1,34 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig([
-  {
-    entry: ["src/index.ts"],
-    format: ["cjs"],
-    dts: false,
-    sourcemap: true,
-    clean: true,
-    target: "es2022",
-    minify: false,
-    outDir: "dist/cjs",
-  },
-  {
-    entry: ["src/index.ts"],
-    format: ["esm"],
-    dts: false,
-    sourcemap: true,
-    clean: false,
-    target: "es2022",
-    minify: false,
-    outDir: "dist/esm",
-  },
-  {
-    entry: ["src/index.ts"],
-    format: ["cjs"],
-    dts: { only: true },
-    sourcemap: false,
-    clean: false,
-    target: "es2022",
-    minify: false,
-    outDir: "dist",
-  },
+   {
+      entry: ["index.ts"],
+      format: ["cjs"],
+      dts: false,
+      sourcemap: true,
+      clean: true,
+      target: "es2022",
+      minify: false,
+      outDir: "dist/cjs",
+   },
+   {
+      entry: ["index.ts"],
+      format: ["esm"],
+      dts: false,
+      sourcemap: true,
+      clean: false,
+      target: "es2022",
+      minify: false,
+      outDir: "dist/esm",
+   },
+   {
+      entry: ["index.ts"],
+      format: ["cjs"],
+      dts: { only: true },
+      sourcemap: false,
+      clean: false,
+      target: "es2022",
+      minify: false,
+      outDir: "dist",
+   },
 ]);
-
-
