6f05fc2e8eb6f6a6f3d746c58e96dfc6a7305cb7
Bar Maor <bmaor2@gmail.com>
2025-08-17 15:16:49 +0300

    feat: setup @example/client

--- example/server/src/main.ts
+++ example/server/src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT ?? 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: true,
   });
-
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT);
+  console.log(`server started on port: ${PORT}`);
 }
 bootstrap();
